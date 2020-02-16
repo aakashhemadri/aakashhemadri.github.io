c3e1a87c61c87735b2c2b9fdcfaa52563be90c86
Aakash Hemadri <aakashhemadri123@gmail.com>
2020-02-16 23:35:32 +0530

    Updated tests.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders my github profile link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Aakash Hemadri/i);
   expect(linkElement).toBeInTheDocument();
 });
