f4efdd184f680ed1f4b97a5e02ace846e2d73f37
Aakash Hemadri <aakashhemadri123@gmail.com>
2020-02-26 01:31:48 +0530

    Rewrite Test

--- src/views/Index/Index.test.js
+++ src/views/Index/Index.test.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getAllByAltText } from "@testing-library/react";
 import Index from "./Index";
 
 test("renders my github profile link", () => {
   const { getByText } = render(<Index />);
-  const linkElement = getByText(/Aakash Hemadri/i);
+  const linkElement = getByText("$ ./Aakash\\ Hemadri");
   expect(linkElement).toBeInTheDocument();
 });
