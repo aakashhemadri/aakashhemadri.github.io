21a5bfb7395680a16654794d3cf8a9c5f181d061
Aakash Hemadri <aakashhemadri123@gmail.com>
2022-02-22 00:39:12 +0530

    fix: pwa
    Signed-off-by: Aakash Hemadri <aakashhemadri123@gmail.com>

--- sw.js
+++ sw.js
@@ -1,16 +1,20 @@
 const staticPage = "aakashhemadri-com"
-const assets = {
+const assets = [
 	"/",
-	"index.html",
-	"style.css",
-	"app.js"
-	"images/",
-}
+	"/index.html",
+	"/style.css",
+	"/app.js",
+	"/images/icons/android-chrome-192x192.png",
+	"/images/icons/android-chrome-512x512.png",
+	"/images/icons/favicon-16x16.png",
+	"/images/icons/favicon-32x32.png",
+	"/images/icons/apple-touch-icon.png",
+]
 
 self.addEventListener("install", installEvent => {
 	installEvent.waitUntil(
 		caches.open(staticPage).then(cache => {
-			cache.AddAll(assets)
+			cache.addAll(assets)
 		})
 	)
 })
